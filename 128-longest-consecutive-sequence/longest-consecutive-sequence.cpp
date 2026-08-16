afcea237a62d650cc9b9d99f2e80c041275b2a70
praneethSSS <113307763+praneethSSS@users.noreply.github.com>
2026-08-16 20:52:42 +0530

    Time: 70 ms (77.65%) | Memory: 89 MB (25.54%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -7,17 +7,30 @@ public:
             st.insert(num);
         }
         int maxi = 0;
+        // for(int num : st){
+        //     if(!st.count(num-1)){
+        //         int current = num;
+        //         int len = 1;
+        //         while(st.count(current+1)){
+        //             current++;
+        //             len++;
+        //         }
+        //         maxi = max(maxi,len);
+        //     }
+        // }
         for(int num : st){
-            if(!st.count(num-1)){
-                int current = num;
+
+            if(!st.count(num+1)){
                 int len = 1;
-                while(st.count(current+1)){
-                    current++;
+                int current = num;
+                while(st.count(current-1)){
+                    current--;
                     len++;
                 }
                 maxi = max(maxi,len);
             }
         }
+
         return maxi;
     }
 };
