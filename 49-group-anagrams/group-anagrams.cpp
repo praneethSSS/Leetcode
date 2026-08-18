860b2f6194b7553b2e9ac734e52c5e11926d4262
praneethSSS <113307763+praneethSSS@users.noreply.github.com>
2026-08-18 07:52:43 +0530

    Time: 75 ms (6.89%) | Memory: 37.5 MB (5.02%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-       unordered_map<string, vector<string>> mp;
+        unordered_map<string,vector<string>> mp;
 
-        for(string s : strs) {
+        
 
-            vector<int> freq(26, 0);
+        for(string str : strs){
 
-            // Count characters
-            for(char c : s) {
+            vector<int> freq(26,0);
+
+            for(char c : str){
                 freq[c - 'a']++;
             }
-
-            // Create frequency signature
-            string key;
-
-            for(int i = 0; i < 26; i++) {
-                key += to_string(freq[i]) + "#";
+            string hash;
+            for(int i=0;i<26;i++){
+                hash += to_string(freq[i]) + "#";
             }
 
-            // Group using the signature
-            mp[key].push_back(s);
+            mp[hash].push_back(str);
         }
-
         vector<vector<string>> ans;
-
         for(auto pair : mp){
             ans.push_back(pair.second);
         }
